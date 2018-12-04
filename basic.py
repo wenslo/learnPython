b254204c407ab692df9233013dbc27bd3aa5d743
wenslo <wenhlo@163.com>
2018-12-04 13:18:40 +0800

    python basic added

---
+++ basic.py
@@ -0,0 +1,47 @@
+print('i\'m ok.')
+print('I\' learning \n Python')
+print('\\\n\\')
+print('\\\t\\')
+print(r'\\\n\\')
+print('''
+line1...
+\tline2...
+\tline3...
+''')
+print(r'''
+line1...
+\tline2...
+\tline3...
+''')
+print(True and False)
+print(False and False)
+print(True or False)
+print(not False)
+a = 0
+print(a)
+t_007 = 't_007'
+print(t_007)
+Answer = True
+print(Answer)
+a = 'ABD'
+b = a
+a = 'XYZ'
+print(b)
+print(10 / 3)
+print(9 / 3)
+print(10 // 3)
+print(10 % 3)
+n = 123
+f = 456.789
+s1 = 'Hello World'
+s2 = 'Hello \'Adam\''
+s3 = r'Hello , "Bart"'
+s4 = r'''
+Hello , Lisa!
+'''
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
