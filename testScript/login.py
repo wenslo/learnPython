b42e7e0c124544e141e096feef242c561d73a48e
wenslo <wenhlo@163.com>
2018-12-08 16:54:56 +0800

    test

--- testScript/login.py
+++ testScript/login.py
@@ -1,13 +1,18 @@
+import json
+
 import requests
 
-LOGIN_URL = 'http://47.105.99.73:8185/v1/aplus-jx-public/universal/user/authentication'
+LOGIN_URL = 'www.baidu.com'
 
 login_response = requests.post(LOGIN_URL, json={
-    "loginName": "tjxadmin1",
-    "password": "123456"
+    "username": "username",
+    "password": "password"
 })
 login_response = login_response.json()
-print('Login resposne is %s' % login_response)
-login_token = login_response['content']['token']
+login_response_formatted = json.dumps(response, sort_keys=True, indent=4, separators=(',', ':'))
+
+print('Login resposne is %s' % login_response_formatted)
+login_token = login_response['token']
 print('The login token is %s ' % login_token)
 
+headers = {'token': login_token}
