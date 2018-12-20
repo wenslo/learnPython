e805f8556ed4c6d50cf186a2af59949e2a01e094
wenhailin <wenhlo@163.com>
2018-12-20 18:28:14 +0800

    test

--- functionly_code.py
+++ functionly_code.py
@@ -47,3 +47,23 @@ def str2int(s):
 
 
 print(str2int('12345'))
+
+
+def is_odd(n):
+    return n % 2 == 1
+
+
+print(list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])))
+
+
+def not_empty(s):
+    return s and s.strip()
+
+
+print(list(filter(not_empty, ['A', '', 'B', None, 'C', ''])))
+
+
+def _odd_iter():
+
+
+
