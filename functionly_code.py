c0b3ffa265e2a7e0adca6cb8657697dcf5960fd9
wenhailin <wenhlo@163.com>
2018-12-21 10:09:00 +0800

    test

--- functionly_code.py
+++ functionly_code.py
@@ -64,6 +64,84 @@ print(list(filter(not_empty, ['A', '', 'B', None, 'C', ''])))
 
 
 def _odd_iter():
+    n = 1
+    while True:
+        n += 2
+        yield n
 
 
+def _not_divisible(n):
+    return lambda x: x % n > 0
 
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+
+# for n in primes():
+#     if n < 1000:
+#         print(n)
+#     else:
+#         break
+
+#
+# def is_palindrome(n):
+#     if isinstance(n, Iterator):
+#         print("not Iterable")
+#         return
+#     for x in n:
+#         if not isinstance(x, int):
+#             continue
+#         num_str = str(x)
+#         length = len(num_str)
+#         # 如果小于10,不进行判断
+#         if length < 2:
+#             print(x)
+#         elif length % 2 == 0:
+#             mid = int(length / 2)
+#             a = num_str[0:mid]
+#             b = num_str[mid:length]
+#             if a == b:
+#                 print(x)
+#         else:
+#             mid = length // 2
+#             a = num_str[0:mid]
+#             b = num_str[mid + 1:length]
+#             if a == b:
+#                 print(x)
+
+
+# is_palindrome(range(1, 200))
+
+
+# 现在改为filter
+def is_palindrome(x):
+    num_str = str(x)
+    length = len(num_str)
+    mid = length // 2
+    a = num_str[0:mid]
+    # 如果小于10,不进行判断
+    if length < 2:
+        return True
+    elif length % 2 == 0:
+        b = num_str[mid:length]
+        if a == b:
+            return True
+    else:
+        b = num_str[mid + 1:length]
+        if a == b:
+            return True
+
+
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101,
+                                                  111, 121, 131, 141, 151, 161, 171, 181, 191]:
+    print('测试成功!')
+else:
+    print('测试失败!')
