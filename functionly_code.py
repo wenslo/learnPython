a0ad6b53120d29ba64d39bc1210111e6c7c07fcd
wenslo <wenhlo@163.com>
2018-12-25 21:27:39 +0800

    function partial added

--- functionly_code.py
+++ functionly_code.py
@@ -403,3 +403,20 @@ elif s != 7986:
     print('测试失败!')
 else:
     print("测试通过！")
+
+print(int('12345', base=8))
+print(int('12345', base=16))
+
+
+def int2(x, base=2):
+    return int(x, base)
+
+
+print(int2('11'))
+
+int2 = functools.partial(int, base=2)
+print(int2('100000'))
+print(int2('10101010'))
+print(int2('1000000', base=10))
+max2 = functools.partial(max, 10)
+print(max2(5, 6, 7))
